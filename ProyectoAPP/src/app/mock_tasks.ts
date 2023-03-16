4b362b0ef06910dadb66e42eb4fccf4877413900
Denise Paoloni <dpaoloni@intranet.bld.com.ar>
2023-03-16 00:10:40 -0300

    Agrgar mock de tareas y el interface de las mismas

---
+++ ProyectoAPP/src/app/mock_tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./task"
+
+export const TASKS :Task[]= [
+    {
+        id: 1,
+        text: "Terminar 1° modulo de Angular",
+        day: 'Agosto 20 23:00',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "Hacer compras del mes",
+        day: 'Agosto 25 16:00',
+        reminder:false
+    },
+    {
+        id: 3,
+        text: "Renovar carnet de conducir",
+        day: 'Marzo 20 7:00',
+        reminder:true
+    },
+    {
+        id: 4,
+        text: "Cumple Fran",
+        day: 'Marzo 25 17:00',
+        reminder:true
+    },
+]
---
+++ ProyectoAPP/src/app/task.ts
@@ -0,0 +1,7 @@
+export interface Task{
+    id: number;// cuando creamos una Tak podria  no tener  el id
+    text: string;
+    day: string;
+    reminder: boolean;
+    
+}
