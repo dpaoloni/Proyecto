acc4ebd915015499074a0aa2207ecb0087f12d98
Denise Paoloni <dpaoloni@intranet.bld.com.ar>
2023-03-16 23:44:20 -0300

    Boton Rojo lista tareas

--- ProyectoAPP/src/app/components/tasks-items/tasks-items.component.ts
+++ ProyectoAPP/src/app/components/tasks-items/tasks-items.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Task } from 'src/app/task';
 import { TASKS } from 'src/app/mock_tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-tasks-items',
@@ -8,7 +9,8 @@ import { TASKS } from 'src/app/mock_tasks';
   styleUrls: ['./tasks-items.component.css']
 })
 export class TasksItemsComponent implements OnInit {
-  @Input() task:Task=TASKS[0]
+  @Input() task: Task = TASKS[0]
+  faTimes=faTimes;
   constructor() { }
 ngOnInit(): void {
   
--- ProyectoAPP/src/app/components/tasks/tasks.component.ts
+++ ProyectoAPP/src/app/components/tasks/tasks.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from 'src/app/task';
 import { TASKS } from 'src/app/mock_tasks';
 
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks:Task[] = TASKS
+  tasks: Task[] = TASKS
+
+
   constructor() { }
 
 ngOnInit(): void {
